8467743b691801d845340cbc768919d3187c9480
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 16:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function memoize(callback, resolver = JSON.stringify) {
-   
+    let cache = new Map();
+  
+    const memoizedFn = (...args) => {
+      const key = resolver(...args);
+  
+      if (cache.has(key)) {
+        return cache.get(key);
+      }
+  
+      const val = callback(...args);
+      cache.set(key, val);
+  
+      return val;
+    };
+  
+    memoizedFn.clear = () => cache.clear();
+  
+    memoizedFn.delete = (...args) => {
+      const key = resolver(...args);
+      cache.delete(key);
+    };
+  
+    memoizedFn.has = (...args) => {
+      const key = resolver(...args);
+      return cache.has(key);
+    };
+  
+    return memoizedFn;
   }
   
   module.exports = memoize;
